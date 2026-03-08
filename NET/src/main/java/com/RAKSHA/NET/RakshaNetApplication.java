95f2b097ef208cd86fbb1ec7b213c0ad810c5495
vaishnavbhosale <vaishnavbharatbhosale@gmail.com>
2026-03-08 10:06:28 +0530

    Initial commit

---
+++ NET/src/main/java/com/RAKSHA/NET/RakshaNetApplication.java
@@ -0,0 +1,13 @@
+package com.RAKSHA.NET;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakshaNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakshaNetApplication.class, args);
+	}
+
+}
---
+++ NET/src/test/java/com/RAKSHA/NET/RakshaNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RAKSHA.NET;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakshaNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
